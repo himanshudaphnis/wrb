ba71fa91a4002d6bc312488e48aa0ee5de701c80
himanshu khare <khareh57@gmail.com>
2018-12-19 10:45:50 +0530

    Form creation

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import ReactDOM from "reactdom";
+import ReactDOM from "react-dom";
+import Input from "../presentational/Input.jsx";
 
 class FormContainer extends Component {
     constructor() {
@@ -8,14 +9,31 @@ class FormContainer extends Component {
         this.state = {
             title: ""
         };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.id]: event.target.value });
     }
 
     render() {
         return(
             <form id="article-form">
+                <Input
+                    text="SEO title"
+                    label="seo_title"
+                    type="text"
+                    id="seo_title"
+                    value={seo_title}
+                    handleChange={this.handleChange}
+                />
             </form>
         );
     }
 }
 
 export default FormContainer;
+
+const wrapper = document.getElementById("create-article-form");
+wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
